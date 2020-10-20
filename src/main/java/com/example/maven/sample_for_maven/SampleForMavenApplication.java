bc9d096a2f31a13ea0ec37ffff82f7add335d6de
ChunKang <ale.kang@gmail.com>
2020-10-20 16:58:30 +0800

    Cerate spring boot project for maven

---
+++ src/main/java/com/example/maven/sample_for_maven/SampleForMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven.sample_for_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleForMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleForMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maven/sample_for_maven/SampleForMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven.sample_for_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleForMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
